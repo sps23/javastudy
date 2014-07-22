274573844c1f2550a80c05f73de5d798fb090066
sylwester.stocki <sylwester.stocki@aps00052718.poland.asseco.corp>
2014-07-22 15:41:05 +0200

    Codility lesson3, task3 with complexity O(N**2).

---
+++ Test Packages/pl/com/silverstar/codility/lesson3/task3/SolutionTest.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.silverstar.codility.lesson3.task3;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ *
+ * @author sylwester.stocki
+ */
+public class SolutionTest {
+
+    private final int MIN_N = 2;
+    private final int MAX_N = 100000;
+    private final int MIN_A = -10000;
+    private final int MAX_A = 10000;
+
+    public SolutionTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+    }
+
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of solution method, of class Solution.
+     */
+    @Test
+    public void testSolutionExample() {
+        System.out.println("testSolutionExample");
+        int[] A = new int[]{4, 2, 2, 5, 1, 5, 8};
+        Solution instance = new Solution();
+        int expResult = 1;
+        int result = instance.solution(A);
+        Assert.assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSolutionExample_Min3Seq() {
+        System.out.println("testSolutionExample_Min3Seq");
+        int[] A = new int[]{9, 2, 7, 5, 7, -1, 5, -3};
+        Solution instance = new Solution();
+        int expResult = 1;
+        int result = instance.solution(A);
+        Assert.assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSolution_MaxN_MaxA() {
+        System.out.println("testSolution_MaxN_MaxA");
+        int[] A = new int[MAX_N];
+        for (int n = 0; n < MAX_N; n++) {
+            A[n] = MAX_A;
+        }
+        Solution instance = new Solution();
+        int expResult = 0;
+        int result = instance.solution(A);
+        Assert.assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testSolution_MaxN_MinA() {
+        System.out.println("testSolution_MaxN_MinA");
+        int[] A = new int[MAX_N];
+        for (int n = 0; n < MAX_N; n++) {
+            A[n] = MIN_A;
+        }
+        Solution instance = new Solution();
+        int expResult = 0;
+        int result = instance.solution(A);
+        Assert.assertEquals(expResult, result);
+    }
+
+}
---
+++ src/pl/com/silverstar/codility/lesson3/task3/Solution.java
@@ -0,0 +1,73 @@
+package pl.com.silverstar.codility.lesson3.task3;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author sylwester.stocki
+ */
+public class Solution {
+
+    public int solution_old(int[] A) {
+
+        int N = A.length;
+        if (N <= 2) {
+            return 0;
+        }
+
+        float minAvg = Float.MAX_VALUE;
+        int minAvgIndex = 0;
+        float[] tmp = new float[N];
+        tmp[0] = A[0];
+        for (int p = 0; p < N - 1; p++) {
+            float tmpSum = A[p];
+            float tmpAvg;
+            float minAvgPerP = Float.MAX_VALUE;
+            for (int q = p + 1; q < N; q++) {
+                tmpSum = tmpSum + A[q];
+                tmpAvg = tmpSum / (q - p + 1);
+                System.out.println("[" + p + ";" + q + "]" + "tmpSum=" + tmpSum + ";tmpAvg=" + tmpAvg);
+                if (tmpAvg < minAvgPerP) {
+                    minAvgPerP = tmpAvg;
+                    tmp[p] = tmpAvg;
+                }
+                if (tmpAvg < minAvg) {
+                    minAvg = tmpAvg;
+                    minAvgIndex = p;
+                }
+            }
+        }
+        System.out.println("A=" + Arrays.toString(A));
+        System.out.println("tmp=" + Arrays.toString(tmp));
+        return minAvgIndex;
+    }
+
+    public int solution(int[] A) {
+        int N = A.length;
+        if (N <= 2) {
+            return 0;
+        }
+
+        float minAvg = Float.MAX_VALUE;
+        int minAvgIndex = 0;
+        int sum = A[0];
+        float avg;
+        int[] tmpSum = new int[N - 1];
+        float[] tmpAvg = new float[N - 1];
+
+        int p = 0;
+        int q = p + 1;
+        for (; q < N - 1; q++) {
+            sum += A[q];
+            tmpSum[q - 1] = sum;
+            avg = sum / (q - p + 1);
+            tmpAvg[q - 1] = avg;
+        }
+        System.out.println("A=" + Arrays.toString(A));
+        System.out.println("tmpSum=" + Arrays.toString(tmpSum));
+        System.out.println("tmpAvg=" + Arrays.toString(tmpAvg));
+
+        return minAvgIndex;
+    }
+
+}
